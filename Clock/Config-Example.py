f2d0e002a9b60cf59ef01342d4538b5745f25552
Nick D <ndileo4@gmail.com>
2015-06-23 22:27:24 -0400

    Initial Checkin

---
+++ Clock/Config-Example.py
@@ -0,0 +1,68 @@
+from GoogleMercatorProjection import LatLng
+
+wuprefix = 'http://api.wunderground.com/api/'
+wulocation = LatLng(44.9764016,-93.2486732)
+noaastream = 'http://audioplayer.wunderground.com:80/tim273/edina'
+background = 'images/clockbackground-kevin.png'
+squares1 = 'images/squares1-kevin.png'
+squares2 = 'images/squares2-kevin.png'
+icons = 'icons-lightblue'
+textcolor = '#bef'
+clockface = 'images/clockface3.png'
+hourhand = 'images/hourhand.png'
+minhand = 'images/minhand.png'
+sechand = 'images/sechand.png'
+
+metric = 0  #0 = English, 1 = Metric
+radar_refresh = 10      # minutes
+weather_refresh = 30    # minutes
+
+radar1 = {
+    'center' : LatLng(44.9764016,-93.2486732),
+    'zoom' : 7,
+    'markers' : (
+        {
+        'location' : LatLng(44.9764016,-93.2486732),
+        'color' : 'red',
+        'size' : 'small',
+        },
+        )
+    }
+
+    
+radar2 = {
+    'center' : LatLng(44.9764016,-93.2486732),
+    'zoom' : 11,
+    'markers' : (
+        {
+        'location' : LatLng(44.9764016,-93.2486732),
+        'color' : 'red',
+        'size' : 'small',
+        },
+        )
+    }
+
+    
+radar3 = {
+    'center' : LatLng(44.9764016,-93.2486732),
+    'zoom' : 7,
+    'markers' : (
+        {
+        'location' : LatLng(44.9764016,-93.2486732),
+        'color' : 'red',
+        'size' : 'small',
+        },
+        )
+    }
+
+radar4 = {
+    'center' : LatLng(44.7212951,-93.2008627),
+    'zoom' : 11,
+    'markers' : (
+        {
+        'location' : LatLng(44.7212951,-93.2008627),
+        'color' : 'red',
+        'size' : 'small',
+        },
+        )
+    }
---
+++ startup.sh
@@ -0,0 +1,47 @@
+# Startup script for the PiClock
+# Designed to be started from crontab as follows
+#@reboot sh /home/pi/PiClock/startup.sh
+
+#
+cd /home/pi/PiClock
+
+# wait for Xwindows and the desktop to start up
+sleep 45
+
+# stop screen blanking
+export DISPLAY=:0.0
+xset s off
+xset -dpms
+xset s noblank
+
+# get rid of mouse cursor
+unclutter &
+
+# push sound level to maximum
+amixer cset numid=1 -- 400
+
+# NeoPixel AmbiLights
+cd Leds
+sudo python NeoAmbi.py &
+cd ..
+
+# gpio button to keyboard input
+sudo Button/gpio-keys 23:KEY_SPACE 24:KEY_F2 25:KEY_UP &
+
+# for temperature sensor(s) on One Wire bus
+cd Temperature
+python TempServer.py &
+cd ..
+
+# the main app
+cd Clock
+# create a new log file name, max of 7 log files
+lc=`expr $(cat log.count 2>/dev/null) + 1`
+if [ "$lc" -gt "7" ]
+then
+  lc=1
+fi
+echo $lc >log.count
+lf="PyQtPiClock.$lc.log"
+# start the clock
+python -u PyQtPiClock.py >$lf 2>&1
